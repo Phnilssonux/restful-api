51ff7a7159e4474e6157d89d76dc72a50e1e6321
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2025-03-27 13:56:29 +0100

    finished assignment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
 
 const PORT = 3000;
@@ -10,7 +9,7 @@ app.listen(PORT, () => {
     console.log(`server is running in http://localhost:${PORT}`);
 });
 
-let users: [
+let users = [
     {
         id: 1, name: "Philip", gender: "male",
     },
@@ -25,10 +24,34 @@ app.get("/users", (req, res) => {
 
 app.post("/users", (req, res) => {
     const newUser = {
-        id: users.length +1,
-        title: req.body.name,
+        id: users.length + 1,
+        name: req.body.name,
         gender: req.body.gender,
     };
     users.push(newUser);
     res.json({ message: "User added succesfully!", user: newUser});
+});
+
+app.put("/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+    if(!user) {
+        return res.status(404).json({message: "User not found!"});
+    }
+    user.name = req.body.name || user.name;
+    user.gender = req.body.gender || user.gender;
+    res.json({message: "User updated successfully!", user });
+});
+
+app.delete("/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find((u) => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found!" });
+    }
+
+    users = users.filter((u) => u.id !== userId);
+
+    res.json({ message: "User deleted successfully!", deletedUser: user });
 });
